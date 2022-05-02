9d40ee4c0660156beb5fa1ca99f751b733645b2f
Maryann <mwikali119@gmail.com>
2022-05-02 19:15:41 +0300

    Define logic to be dislayed by API

--- Mgt_App/views.py
+++ Mgt_App/views.py
@@ -4,10 +4,25 @@ import requests
 # Create your views here.
 
 def future(request):
-    url = 'http://api.weatherapi.com/v1/current.json?key=22ace5ac6ed74aa397c195726220105&q=Nairobi&aqi=no'
-    city = 'Nairobi'
+    url = 'http://api.weatherapi.com/v1/forecast.json?key=22ace5ac6ed74aa397c195726220105&q=London&days=10&aqi=no&alerts=no'
 
     response = requests.get(url)
-    print(response.text)
+    # print(response.text)
     r = response.json()
-    return render(request,'future.html',{"r":r})
+    general = {
+        'country': r['location']['country'],
+        'city' : r ['location']['region'],
+        'latitude':r['location']['lat'] ,
+        'longitude':r['location']['lon'] ,
+        'condition' : r['current']['condition']['text'],
+        'image' : r['current']['condition']['icon'],
+    }
+    forecast = {
+        'Average Temperature in Celsius':r['forecast']['forecastday'][0]['day']['avgtemp_c'] ,
+        'Total Rainfall in mm':r['forecast']['forecastday'][0]['day']['totalprecip_mm'] ,
+        'Average Humidity': r['forecast']['forecastday'][0]['day']['avghumidity'] ,
+    }
+
+    # print(forecast)
+
+    return render(request,'future.html',{"general":general,"forecast":forecast})
--- Mgt_Project/settings.py
+++ Mgt_Project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Mgt_App',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
