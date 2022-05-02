019463aff08542c3187b31b6438c13d60bbc9b15
Maryann <mwikali119@gmail.com>
2022-05-02 15:59:31 +0300

    At this point I can print weather data from API

--- Mgt_App/views.py
+++ Mgt_App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def future(request):
+    url = 'http://api.weatherapi.com/v1/current.json?key=22ace5ac6ed74aa397c195726220105&q=Nairobi&aqi=no'
+    city = 'Nairobi'
+
+    response = requests.get(url)
+    print(response.text)
+    r = response.json()
+    return render(request,'future.html',{"r":r})
